823d3b03c04f88ae922fc3750472f4dcc8086f65
Sonal0877 <103572026+Sonal0877@users.noreply.github.com>
2022-04-13 17:14:30 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,20 @@
+def cal_factorial(number: int) -> int:
+    """
+    Calculate the factorial of provided number
+    :param number: input number
+    :return: factorial
+    """
+    output = 1
+    if number == 0:
+        return 1
+    elif number != 0:
+        for num in range(1, number+1):
+            output *= num
+        return output
+
+
+for num in range(36):
+    factorial = cal_factorial(num)
+    print("{} {}".format(num,factorial))
+
+
